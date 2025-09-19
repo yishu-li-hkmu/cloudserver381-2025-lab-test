7fd51a8810ea99aa9bceef5e2b66ac9cc63ba302
yishu-li-hkmu <sliy@hkmu.edu.hk>
2025-09-19 18:05:39 +0800

    Add files via upload

---
+++ test_01/task2/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8099
+
+app.get('/', (req, res) => {
+  res.send('Connection successfully for my task 2!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
